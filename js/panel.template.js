564d3717a05cfe3aa689af199886244daf98dc0f
chengwb <chengwb53@qq.com>
2016-09-09 19:15:51 +0800

    ss

--- js/panel.template.js
+++ js/panel.template.js
@@ -6,6 +6,7 @@
 	'use strict';
 
 	function PanelTemplate(root, options) {
+		var templates = [];//暂时保存模板，以名字为id
 		var defaults = {
 			xCells: 4,
 			yCells: 3,
@@ -25,9 +26,10 @@
 		var width = 1200;
 		var height = 900;
 		var position = {};
-		var modules = {};
+		var modules = [];
 		var moduleIndex = 0;
 		var self = this;
+		var templateEvent;
 
 		modules.length = 0;
 		options.xCells = parseInt(options.xCells);
@@ -76,42 +78,106 @@
 
 				return null;
 			},
+			lineMove: function (sourceModule, targetCell) {
+				var moveInfo = {
+					modules: [],
+					xMove: 0,
+					yMove: 0
+				};
+				var targetAreaModules = [];
+				targetAreaModules.length = 0;
+
+				//所有完整
+				function checkFullModule(xMove, yMove) {
+					var start = targetCell;//移动小于0时
+					if (xMove > 0) {
+						start = {
+							xIndex: sourceModule.xIndex + sourceModule.xCells,
+							yIndex: sourceModule.yIndex
+						}
+					} else if (yMove > 0) {
+						start = {
+							xIndex: sourceModule.xIndex,
+							yIndex: sourceModule.yIndex + sourceModule.yCells
+						}
+					}
+
+					var yCells = Math.abs(yMove);
+					var xCells = Math.abs(xMove);
+					if (xCells !== 0) {
+						yCells = sourceModule.yCells;
+					} else if (yCells !== 0) {
+						xCells = sourceModule.xCells;
+					}
+
+					for (var y = 0; y < yCells; y++) {
+						for (var x = 0; x < xCells; x++) {
+							var cell = panelUsage.info[start.yIndex + y][start.xIndex + x];
+
+							if (!cell.useInfo.moduleId) {
+								continue;
+							}
+
+							//是不是模板的第一个单元块
+							if (cell.useInfo.xIndex === 0 && cell.useInfo.yIndex === 0) {
+								//完整性
+								var targetAreaModule = modules[cell.useInfo.moduleId];
+
+								if (targetAreaModule.xCells > (xCells - x) ||
+									targetAreaModule.yCells > (yCells - y)) {
+									//不完整
+									return false;
+								}
+
+								targetAreaModules[targetAreaModule.id] = targetAreaModule;
+								targetAreaModules.length += 1;
+							} else {
+								//如果不是第一个单元块，但完整块中已有该模块，则继续
+								if (targetAreaModules[cell.useInfo.moduleId]) {
+									continue;
+								}
+
+								return false;
+							}
+						}
+					}
+
+					return true;
+				}
+
+				/**4种移动，判断移动区域内的模块是否是完整的，如果是完整
+				 * 的则把移动区域内所有模块反向移动当前移动块的大小（根据方向来确定是x还是y）
+				 * 并把移动块放下
+				 */
+				if (sourceModule.xIndex === targetCell.xIndex ||
+					sourceModule.yIndex === targetCell.yIndex) {
+					var yMove = targetCell.yIndex - sourceModule.yIndex;
+					var xMove = targetCell.xIndex - sourceModule.xIndex;
+					moveInfo.xMove = xMove > 0 ? 1 : (xMove < 0 ? -1 : 0);
+					moveInfo.yMove = yMove > 0 ? 1 : (yMove < 0 ? -1 : 0);
+
+					if (checkFullModule(xMove, yMove)) {
+						moveInfo.modules = targetAreaModules;
+						return moveInfo;
+					} else {
+						return false;
+					}
+				} else {
+					//不是水平或垂直移动，这里不处理
+					return false;
+				}
+			},
 			isValidPosition: function (sourceModule, targetCell) {
 				var self = this;
 				var targetAreaModules = [];
-				var intersects = [];
 				targetAreaModules.length = 0;
 
-				//判断是否相交，如果相交判断目标区域相同位置是否有模块
+				//判断是否相交，如果相交则返回目标区域对应索引坐标
 				function isIntersect(cell) {
-					//源模块四个角的cell索引坐标
-					//var points = [];
-					//points.push({xIndex: sourceModule.xIndex, yIndex: sourceModule.yIndex});
-					//points.push({xIndex: sourceModule.xIndex + sourceModule.xCells, yIndex: sourceModule.yIndex});
-					//points.push({xIndex: sourceModule.xIndex, yIndex: sourceModule.yIndex + sourceModule.yCells});
-					//points.push({xIndex: points[1].xIndex, yIndex: points[2].yIndex});
-					//
-					////目标区域
-					//var startXIndex = targetCell.xIndex;
-					//var endXIndex = targetCell.xIndex + sourceModule.xCells;
-					//var startYIndex = targetCell.yIndex;
-					//var endYIndex = targetCell.yIndex + sourceModule.yCells;
-					//
-					//for(var i = 0; i < points.length; i++) {
-					//	var point = points[i];
-					//
-					//	if (point.xIndex >= startXIndex && point.xIndex <= endXIndex &&
-					//		point.yIndex >= startYIndex && point.yIndex <= endYIndex) {
-					//		return true;
-					//	}
-					//}
-					//
-					//return false;
-
-					for(var y = 0; y < sourceModule.yCells; y++) {
-						for(var x = 0; x < sourceModule.xCells; x++) {
+					for (var y = 0; y < sourceModule.yCells; y++) {
+						for (var x = 0; x < sourceModule.xCells; x++) {
 							var sCell = panelUsage.info[sourceModule.yIndex + y][sourceModule.xIndex + x];
-							if(cell.xIndex === sCell.xIndex && cell.yIndex === sCell.yIndex) {
+							if (cell.xIndex === sCell.xIndex && cell.yIndex === sCell.yIndex) {
 								return {
 									xIndex: targetCell.xIndex + x,
 									yIndex: targetCell.yIndex + y
@@ -122,20 +188,15 @@
 
 					return false;
 				}
-				//intersects = isIntersect();
-				////待处理，获取具体相交的位置，判断目标区域该位置是否有模块
-				//if(intersects) {
-				//	return false;
-				//}
 
 				//判断是否是：在相交处源模块位置对应到目标模块的相应位置上
-				for(var y = 0; y < sourceModule.yCells; y++) {
-					for(var x = 0; x < sourceModule.xCells; x++) {
+				for (var y = 0; y < sourceModule.yCells; y++) {
+					for (var x = 0; x < sourceModule.xCells; x++) {
 						var tCell = panelUsage.info[targetCell.yIndex + y][targetCell.xIndex + x];
 						var result = isIntersect(tCell);
-						if(result){
+						if (result) {
 							var useInfo = panelUsage.info[result.yIndex][result.xIndex].useInfo;
-							if(useInfo.moduleId && useInfo.moduleId !== sourceModule.id) {
+							if (useInfo.moduleId && useInfo.moduleId !== sourceModule.id) {
 								return false;
 							}
 						}
@@ -147,19 +208,25 @@
 						var cell = self.info[targetCell.yIndex + y][targetCell.xIndex + x];
 
 						//如果是自己则忽略，不是自己则继续判断
-						if (cell.useInfo.moduleId && cell.useInfo.moduleId !== sourceModule.id) {
+						if (cell.useInfo.moduleId &&
+							cell.useInfo.moduleId !== sourceModule.id) {
+							//不能交换则结束
+							if (!modules[cell.useInfo.moduleId].swap) {
+								return false;
+							}
+
 							//是不是模板的第一个单元块
-							if(cell.useInfo.xIndex === 0 && cell.useInfo.yIndex === 0) {
+							if (cell.useInfo.xIndex === 0 && cell.useInfo.yIndex === 0) {
 								//完整性
 								var targetAreaModule = modules[cell.useInfo.moduleId];
 
-								if(targetAreaModule.xCells > (sourceModule.xCells - x) ||
+								if (targetAreaModule.xCells > (sourceModule.xCells - x) ||
 									targetAreaModule.yCells > (sourceModule.yCells - y)) {
 									return false;
 								}
 
 								targetAreaModules[targetAreaModule.id] = {
-									module:targetAreaModule,
+									module: targetAreaModule,
 									relativeIndex: {
 										x: x,
 										y: y
@@ -167,7 +234,7 @@
 								};
 								targetAreaModules.length += 1;
 							} else {
-								if(targetAreaModules[cell.useInfo.moduleId]){
+								if (targetAreaModules[cell.useInfo.moduleId]) {
 									continue;
 								}
 
@@ -249,6 +316,57 @@
 				module.xIndex = target.xIndex;
 				module.yIndex = target.yIndex;
 			},
+			lineSwapModule: function(sourceModule, moveInfo, targetCell) {
+				//源模块区域重置
+				for (var y = 0; y < sourceModule.yCells; y++) {
+					for (var x = 0; x < sourceModule.xCells; x++) {
+						var sourceCell = panelUsage.info[sourceModule.yIndex + y][sourceModule.xIndex + x];
+						sourceCell.clean();
+					}
+				}
+
+				function updateModel(module, position) {
+					for (var y = 0; y < module.yCells; y++) {
+						for (var x = 0; x < module.xCells; x++) {
+							var cell = panelUsage.info[position.yIndex + y][position.xIndex + x];
+
+							cell.useInfo.moduleId = module.id;
+							cell.useInfo.xIndex = x;
+							cell.useInfo.yIndex = y;
+						}
+					}
+				}
+
+				//更新目标区域
+				updateModel(sourceModule, targetCell);
+
+				var xMove = moveInfo.xMove * sourceModule.xCells;
+				var yMove = moveInfo.yMove * sourceModule.yCells;
+				for (var key in moveInfo.modules) {
+					var module = moveInfo.modules[key];
+					//相反方向移动
+					var cell = panelUsage.info[module.yIndex - yMove][module.xIndex - xMove];
+					var position = {
+						xIndex: cell.xIndex,
+						yIndex: cell.yIndex
+					};
+
+					//区域更新
+					updateModel(module, position);
+
+					//模块信息更新
+					module.x = cell.x;
+					module.y = cell.y;
+					module.xIndex = cell.xIndex;
+					module.yIndex = cell.yIndex;
+				}
+
+				//模块信息更新
+				sourceModule.x = targetCell.x;
+				sourceModule.y = targetCell.y;
+				sourceModule.xIndex = targetCell.xIndex;
+				sourceModule.yIndex = targetCell.yIndex;
+			},
 			swapModule: function (sourceModule, targetModules, targetCell) {
 				//源模块区域重置
 				for (var y = 0; y < sourceModule.yCells; y++) {
@@ -259,8 +377,8 @@
 				}
 
 				function updateModel(module, position) {
-					for(var y = 0; y < module.yCells; y++) {
-						for(var x = 0; x < module.xCells; x++) {
+					for (var y = 0; y < module.yCells; y++) {
+						for (var x = 0; x < module.xCells; x++) {
 							var cell = panelUsage.info[position.yIndex + y][position.xIndex + x];
 
 							cell.useInfo.moduleId = module.id;
@@ -273,7 +391,7 @@
 				//更新目标区域
 				updateModel(sourceModule, targetCell);
 				//更新源区域
-				for(var key in targetModules) {
+				for (var key in targetModules) {
 					var targetModule = targetModules[key];
 					var position = {
 						xIndex: sourceModule.xIndex + targetModule.relativeIndex.x,
@@ -340,6 +458,69 @@
 			console.log(axis);
 		}
 
+		function refreshPanelInfo() {
+			var cells = $panel.find('.cell');
+			var counts = 0;
+
+			var oldPanelX = panelUsage.info[0].length;
+			var oldPanelY = panelUsage.info.length;
+			for (var y = 0; y < options.yCells; y++) {
+				var yAxis = [];
+				for (var x = 0; x < options.xCells; x++) {
+					var point = null;
+					if (y < oldPanelY && x < oldPanelX) {
+						point = panelUsage.info[y][x];
+					}
+					var position = $(cells[counts++]).position();
+
+					if (point) {
+						point.x = position.left;
+						point.y = position.top;
+					} else {
+						if (y < oldPanelY && x > oldPanelX) {
+							panelUsage.info[y].push({
+								x: position.left,
+								y: position.top,
+								useInfo: {
+									moduleId: null,
+									xIndex: 0,
+									yIndex: 0
+								},
+								xIndex: x,
+								yIndex: y,
+								clean: function () {
+									this.useInfo.moduleId = null;
+									this.useInfo.xIndex = 0;
+									this.useInfo.yIndex = 0;
+								}
+							});
+						} else {
+							yAxis.push({
+								x: position.left,
+								y: position.top,
+								useInfo: {
+									moduleId: null,
+									xIndex: 0,
+									yIndex: 0
+								},
+								xIndex: x,
+								yIndex: y,
+								clean: function () {
+									this.useInfo.moduleId = null;
+									this.useInfo.xIndex = 0;
+									this.useInfo.yIndex = 0;
+								}
+							});
+						}
+					}
+				}
+
+				if (yAxis.length > 0) {
+					panelUsage.info.push(yAxis);
+				}
+			}
+		}
+
 		function onEvent() {
 			var module = {
 				clean: function () {
@@ -365,7 +546,7 @@
 					$panel.find('.tipBox').removeClass('second_top');
 				},
 				startDrag: function (event) {
-					var $target = $(event.target);
+					var $target = $(event.target).parents('.module').eq(0);
 					//判断是否可拖动
 					if ($target.attr('data-drag') !== 'true') {
 						return;
@@ -375,7 +556,7 @@
 					var offset = $target.offset();
 					$target.addClass('drag');
 
-					this.target = event.target;
+					this.target = $target;
 
 					//模块相对模板左上角的位置
 					this.x = position.left;
@@ -455,11 +636,11 @@
 						var results = panelUsage.isValidPosition(sourceModule, targetCell);
 						if (results) {
 							//完整模块为0个，直接放
-							if(results.length === 0) {
+							if (results.length === 0) {
 								panelUsage.updateModule(sourceModule, targetCell);
 								position.x = targetCell.x;
 								position.y = targetCell.y;
-							}else{
+							} else {
 								//多个完整模块，等位交换位置
 								panelUsage.swapModule(sourceModule, results, targetCell);
 
@@ -467,7 +648,7 @@
 								position.y = targetCell.y;
 
 								//遍历移动
-								for(var key in results) {
+								for (var key in results) {
 									$panel.find('.module[data-id=' + key + ']').stop(true).animate({
 										top: modules[key].y + 'px',
 										left: modules[key].x + 'px'
@@ -475,11 +656,37 @@
 								}
 							}
 						} else {
-							//还原
-							position.x = sourceModule.x;
-							position.y = sourceModule.y;
+							//查看是否是水平或直线移动
+							var moveInfo = panelUsage.lineMove(sourceModule, targetCell);
+							if (moveInfo) {
+								//进这儿了必然有数据，如果没数据，前面的算法已处理，不会到这个分支
+								position.x = targetCell.x;
+								position.y = targetCell.y;
+
+								//遍历移动
+								var xMove = moveInfo.xMove * sourceModule.xCells;
+								var yMove = moveInfo.yMove * sourceModule.yCells;
+								for (var key in moveInfo.modules) {
+									var module = moveInfo.modules[key];
+									//相反方向移动
+									var cell = panelUsage.info[module.yIndex - yMove][module.xIndex - xMove];
+
+									$panel.find('.module[data-id=' + key + ']').stop(true).animate({
+										top: cell.y + 'px',
+										left: cell.x + 'px'
+									}, 200);
+								}
+
+								//多个完整模块，直线交换位置
+								panelUsage.lineSwapModule(sourceModule, moveInfo, targetCell);
+							} else {
+								//还原
+								position.x = sourceModule.x;
+								position.y = sourceModule.y;
+							}
 						}
 					}
+
 					changePosition();
 
 					$(this.target).stop(true).animate({
@@ -499,21 +706,42 @@
 				}
 			};
 
-			$panel.on('mousedown', '.module', function (event) {
+			function onStartDrag(event) {
 				module.startDrag(event);
-			});
+			}
 
-			$(document).on('mousemove', function (event) {
+			function onDraging(event) {
 				module.onDrag(event);
-			});
+			}
 
-			$(document).on('mouseup', function (event) {
+			function onEndDrag(event) {
 				module.endDrag(event);
-			});
+			};
+
+			$panel.on('mousedown', '.module > .cover', onStartDrag);
+			$(document).on('mousemove', onDraging);
+			$(document).on('mouseup', onEndDrag);
+
+			return {
+				off: function () {
+					$panel.off('mousedown', '.module > .cover', onStartDrag);
+					$(document).off('mousemove', onDraging);
+					$(document).off('mouseup', onEndDrag);
+				}
+			}
 		}
 
 		function render() {
+			init();
+
+			$panel.empty();
 			renderCell();
+			if (modules.length === 0 || panelUsage.info.length === 0) {
+				initPanelInfo();
+			} else {
+				refreshPanelInfo();
+			}
+
 			renderTipBox();
 			renderModules();
 		}
@@ -540,34 +768,65 @@
 
 		function renderModule(module) {
 			$panel.append('<div class="module" ondragstart="return false" ondrag="return false" data-id="' + module.id + '">' +
-				'<span class="delete_btn" ondragstart="return false" ondrag="return false">x</span></div>');
+				'<span class="delete_btn" ondragstart="return false" ondrag="return false">x</span>' +
+				'<iframe src="' + module.url + '"></iframe>' +
+				'<span class="cover"></span>' +
+				'</div>');
 
+			//var position = panelUsage.info[module.yIndex][module.xIndex];
 			//-6是border，为了展示块之间的间隔效果
 			$('.module[data-id=' + module.id + ']').css({
-				width: module.width - 6,
-				height: module.height - 6,
-				top: module.y,
-				left: module.x
+				width: width / options.xCells * module.xCells - 6,
+				height: height / options.yCells * module.yCells - 6,
+				top: height / options.yCells * module.yIndex,
+				left: width / options.xCells * module.xIndex
 			}).attr({
 				'data-drag': module.drag ? 'true' : 'false',
 				'data-swap': module.swap ? 'true' : 'false'
 			});
+
+			//
+			$('.module[data-id=' + module.id + '] > iframe').css({
+				width: width / options.xCells * module.xCells - 6,
+				height: height / options.yCells * module.yCells - 6,
+				top: height / options.yCells * module.yIndex,
+				left: width / options.xCells * module.xIndex
+			});
+
+			console.log(panelUsage.info);
+			console.log(modules);
 		}
 
 		function renderModules() {
 			//遍历modules，根据位置设置
-			//renderModule();
+			if (modules.length === 0) {
+				return;
+			}
+
+			for (var key in modules) {
+				renderModule(modules[key]);
+			}
 		}
 
-		this.refresh = function () {
+		/**
+		 * 校正useInfo信息
+		 */
+		function regulateUseInfo() {
+			for(var key in modules) {
+				var module = modules[key];
+				panelUsage.updateModule(module, panelUsage.info[module.yIndex][module.xIndex]);
+			}
+		}
+
+		this.refresh = function (xCells, yCells) {
+			setRate(xCells, yCells);
 			render();
 		};
 
-		this.setRate = function (x, y) {
+		function setRate(x, y) {
 			options.xCells = x;
 			options.yCells = y;
-			//重新计算，绘图
-		};
+		}
 
 		this.createMoudle = function (params) {
 			var position = {
@@ -595,10 +854,10 @@
 			var moduleId = 'module' + moduleIndex;
 			var module = {
 				id: moduleId,
+				name: params.name,
+				url: params.url,
 				xCells: params.xCells,
 				yCells: params.yCells,
-				width: width / options.xCells * params.xCells,
-				height: height / options.yCells * params.yCells,
 				drag: params.drag,
 				swap: params.swap,
 				x: position.x,
@@ -629,17 +888,99 @@
 			$panel.find('.module').remove();
 			panelUsage.cleanAllModule();
 
-			modules = {};
+			modules = [];
 			modules.length = 0;
 		};
 
 		this.save = function () {
 			//转换成后台需要的结构，然后ajax上传
+			var template = {
+				name: options.name,
+				xCells: options.xCells,
+				yCells: options.yCells,
+				modules: []
+			};
+
+			for(var key in modules) {
+				template.modules.push(modules[key]);
+			}
+
+			//存在则更新
+			for(var i = 0; i < templates.length; i++) {
+				if(templates[i].name === options.name) {
+					templates[i] = template;
+					return;
+				}
+			}
+
+			//新加
+			templates.push(template);
 		};
 
 		this.delete = function () {
 			this.clean();
 			$panel.empty();
+
+			templateEvent.off();
+		};
+
+		this.show = function(name) {
+			modules = [];
+			modules.length = 0;
+			panelUsage.info = [];
+
+			//纯展示
+			for(var i = 0; i < templates.length; i++) {
+				var template = templates[i];
+				if(template.name === name) {
+					options.name = template.name;
+					options.xCells = template.xCells;
+					options.yCells = template.yCells;
+					for(var j = 0; j < template.modules.length; j++){
+						modules.length += 1;
+						modules[template.modules[j].id] = template.modules[j];
+					}
+
+					init();
+					$panel.empty();
+					renderModules();
+
+					templateEvent.off();
+					return template;
+				}
+			}
+
+			return false;
+		};
+
+		this.edit = function(name) {
+			modules = [];
+			modules.length = 0;
+			panelUsage.info = [];
+
+			for(var i = 0; i < templates.length; i++) {
+				var template = templates[i];
+				if(template.name === name) {
+					options.name = template.name;
+					options.xCells = template.xCells;
+					options.yCells = template.yCells;
+					for(var j = 0; j < template.modules.length; j++){
+						modules.length += 1;
+						modules[template.modules[j].id] = template.modules[j];
+					}
+
+					templateEvent.off();
+					templateEvent = onEvent();
+					render();
+
+					//处理panelUseInfo
+					regulateUseInfo();
+
+					return template;
+				}
+			}
+
+			return false;
 		};
 
 		$panel.on('click', '.delete_btn', function (event) {
@@ -647,10 +988,9 @@
 			self.deleteMoudle(index);
 		});
 
-		init();
 		render();
-		initPanelInfo();
-		onEvent();
+
+		templateEvent = onEvent();
 	}
 
 	global.PanelTemplate = global.PanelTemplate || PanelTemplate;
